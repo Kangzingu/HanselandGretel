d7f3c0b5b96d0ed79a3cb7ce702da8b8b2776b31
Kangzingu <34128052+Kangzingu@users.noreply.github.com>
2018-11-07 01:20:24 +0900

    수정

---
+++ 18_10_31/Assets/Scripts/MenuChildController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuChildController : MonoBehaviour {
+    public GameObject child;
+    public Vector3 startLocation;
+    public Vector3 endLocation;
+    // Use this for initialization
+	void Start () {
+        startLocation = child.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (child.transform.position.x > endLocation.x && child.transform.position.z > endLocation.z)
+        {
+            child.transform.position = startLocation;
+        }
+	}
+}
