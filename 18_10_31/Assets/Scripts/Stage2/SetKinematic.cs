89f18b9411f5e09caebac3c2c929dbeb57448a2b
Kangzingu <34128052+Kangzingu@users.noreply.github.com>
2018-11-10 00:37:33 +0900

    수정

---
+++ 18_10_31/Assets/Scripts/Stage2/SetKinematic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetKinematic : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            collision.collider.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            collision.collider.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+        //    collision.collider.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
