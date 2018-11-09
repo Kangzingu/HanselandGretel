22ac300955d7fd386ac87197fbfe5ebf9026d9e5
tlaektmf <dazzel0826@gmail.com>
2018-11-10 06:21:25 +0900

    [효과음 stage2에 넣기][열]

--- 18_10_31/Assets/Scripts/Stage2/AudioAdjust.cs
+++ 18_10_31/Assets/Scripts/Stage2/AudioAdjust.cs
@@ -8,13 +8,18 @@ public class AudioAdjust : MonoBehaviour {
     bool startTutorial;
 
     public AudioClip Tutorial;
+    public AudioClip subway;
+    public AudioClip afterSubway;
     AudioSource aud;
-    
+    bool hereSubway;
+    bool nosubwayMusic;
 
 	// Use this for initialization
 	void Start () {
         startTutorial = true;
         this.aud = GetComponent<AudioSource>();
+        hereSubway = true;
+        nosubwayMusic = false;
     }
 	
 	// Update is called once per frame
@@ -24,12 +29,57 @@ public class AudioAdjust : MonoBehaviour {
         if (startTutorial == true)
         {
             //tutorial audio 재생
+            Debug.Log("tutorial 재생");
             this.aud.PlayOneShot(this.Tutorial);
             startTutorial = false;
         }
-        
 
+        if(hereSubway)
+            hereSubway=musicChange();
 
+        if (hereSubway)
+        {
+            Debug.Log("subway음악 재생");
+            if (!this.aud.isPlaying)
+            {
+                this.aud.PlayOneShot(this.subway);
+            }
+ 
+        }
+        else
+        {
+            if (nosubwayMusic)
+            {
+                this.aud.Stop();
+                nosubwayMusic = false;
+            }
 
-	}
+            Debug.Log("시끌시끌 소리 재생");
+            if (!this.aud.isPlaying)
+            {
+                this.aud.PlayOneShot(afterSubway);
+            }
+           
+        }
+
+    }
+    public bool musicChange()
+    {
+        Transform player;
+
+        player = Player.transform;
+        Vector3 flag = new Vector3((float)-14.78, (float)7.7, (float)0.168);
+        float distance = Vector3.Distance(player.position, flag);
+
+        if (distance <= 6.0f)
+        {
+            return false;
+        }
+        else
+        {
+            nosubwayMusic = true;
+            return true;
+
+        }
+    }
 }
