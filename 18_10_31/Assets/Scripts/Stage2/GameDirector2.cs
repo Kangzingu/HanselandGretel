e22db96536b9acf94f50483087ccfadc2c704cc5
Kangzingu <34128052+Kangzingu@users.noreply.github.com>
2018-11-09 10:35:03 +0900

    수정

--- 18_10_31/Assets/Scripts/Stage2/GameDirector2.cs
+++ 18_10_31/Assets/Scripts/Stage2/GameDirector2.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 public class GameDirector2 : MonoBehaviour
 {
     // Use this for initialization
-    public GameObject []stopObject=new GameObject[3];
+    public GameObject[] stopObject = new GameObject[3];
     public bool crossState;
     private float changeTime = 5;
-    private float restTime = 0.5f;
     private float countTime;
     void Start()
     {
         crossState = false;
-      
+
         countTime = changeTime;
     }
     // Update is called once per frame
@@ -32,13 +31,20 @@ public class GameDirector2 : MonoBehaviour
         {
             stopObject[0].GetComponent<Collider>().enabled = true;
             stopObject[1].GetComponent<Collider>().enabled = true;
-            stopObject[2].GetComponent<Collider>().enabled = false;
-            stopObject[3].GetComponent<Collider>().enabled = false;
+            if (countTime < changeTime - 0.5f)
+            {
+                stopObject[2].GetComponent<Collider>().enabled = false;
+                stopObject[3].GetComponent<Collider>().enabled = false;
+
+            }
         }
         else
         {
-            stopObject[0].GetComponent<Collider>().enabled = false;
-            stopObject[1].GetComponent<Collider>().enabled = false;
+            if (countTime < changeTime - 0.5f)
+            {
+                stopObject[0].GetComponent<Collider>().enabled = false;
+                stopObject[1].GetComponent<Collider>().enabled = false;
+            }
             stopObject[2].GetComponent<Collider>().enabled = true;
             stopObject[3].GetComponent<Collider>().enabled = true;
         }
